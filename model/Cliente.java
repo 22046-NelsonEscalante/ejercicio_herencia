cdb2d5b81ac89eca20454d0520aa2eb6abbc5350
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2022-09-21 08:24:52 -0600

    Added the Cliente class

---
+++ model/Cliente.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String direccion;
+    private int cantPrest;
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public int getCantPrest() {
+        return cantPrest;
+    }
+    public void setCantPrest(int cantPrest) {
+        this.cantPrest = cantPrest;
+    }   
+}
